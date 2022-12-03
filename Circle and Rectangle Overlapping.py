3a895870d7c3d4e6aa4f233eb0abda95813b35e5
Andrei Klepatch <56235713+HighlyTrainedNeuralNetwork@users.noreply.github.com>
2022-12-02 22:40:55 -0500

    Circle and Rectangle Overlapping solution mod

--- Circle and Rectangle Overlapping.py
+++ Circle and Rectangle Overlapping.py
@@ -2,7 +2,7 @@ def checkOverlap(radius, xCenter, yCenter, x1, y1, x2, y2):
     closestX = min(range(x1, x2 + 1), key=lambda x: abs(xCenter - x))
     closestY = min(range(y1, y2 + 1), key=lambda x: abs(yCenter - x))
     distance = ((closestX - xCenter) ** 2 + (closestY - yCenter) ** 2) ** 0.5
-    if distance <= radius or (xCenter in range(x1, x2) and yCenter in range(y1, y1)):
+    if distance <= radius:
         return True
     return False
 if __name__ == "__main__":
